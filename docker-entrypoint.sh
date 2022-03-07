84e331c2b0420e94149dc91b2bab1549138c7f0b
David Francos <davidfrancos@sipay.es>
2022-03-07 21:05:34 +0100

    Fixed entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 /opt/venv/bin/pan watch --config /etc/config.cfg
 export pid=$!
-poetry run pan serve --config test.conf --port=8080 --host=0.0.0.0 --prefix=$PREFIX
+/opt/venv/bin/pan serve --config test.conf --port=8080 --host=0.0.0.0 --prefix=$PREFIX
 kill -9 $pid
