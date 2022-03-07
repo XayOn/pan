73a9720832fda886c36e3c498d8e9e53d6b9b981
David Francos <davidfrancos@sipay.es>
2022-03-08 00:50:28 +0100

    Fixed typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-pan watch --config /etc/pan/config.cfg
+pan watch --config /etc/pan/config.cfg &
 export pid=$!
 pan serve --config /etc/pan/config.cfg --port=8080 --host=0.0.0.0 --prefix=$PREFIX
 kill -9 $pid
