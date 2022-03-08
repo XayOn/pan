c29081729b8a5f571d80c1674d3a2ce1ded69943
David Francos <davidfrancos@sipay.es>
2022-03-08 01:08:28 +0100

    Test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pan serve --config /etc/pan/config.cfg --port=8080 --host=0.0.0.0 --prefix=$PREFIX &
+pan serve --config /etc/pan/config.cfg --port=8080 --host=0.0.0.0 --prefix=$PREFIX
 export pid=$!
 pan watch --config /etc/pan/config.cfg &
 kill -9 $pid
