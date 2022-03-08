0e7fd5eb35ab7e963a4c5148b53c86475e4a962c
David Francos <davidfrancos@sipay.es>
2022-03-08 01:24:43 +0100

    Base url

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+export BASE_URL=$PREFIX
+[[ $BASE_URL ]] && { cd /app/pan/static && npm install && npm run build; cd /app; }
 pan watch --config /etc/pan/config.cfg &
 export pid=$!
-pan serve --config /etc/pan/config.cfg --port=8080 --host=0.0.0.0 --prefix=$PREFIX -vvv
+pan serve --config /etc/pan/config.cfg --port=8080 --host=0.0.0.0 --prefix=$BASE_URL -vvv
 kill -9 $pid
